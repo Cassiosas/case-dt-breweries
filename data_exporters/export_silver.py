b290b7ecbe425d201b07ac10e0bafdd7838c6c4c
Cassiosas <cassio.sas@gmail.com>
2024-10-02 23:25:00 -0300

    Update export_silver.py

--- data_exporters/export_silver.py
+++ data_exporters/export_silver.py
@@ -6,14 +6,14 @@ if 'data_exporter' not in globals():
 @data_exporter
 def export_data(df, *args, **kwargs):
     storage_options = {
-        'AZURE_STORAGE_ACCOUNT_NAME': 'datalakebreweries',
-        'AZURE_STORAGE_ACCOUNT_KEY': 'MQAB8GccKbGAwKOQ7YxEy+nrRYC8/M4h+uUMLYKI0ePxNgSNReNWDre1c9ilzY3wwJi5xSmWhsTL+AStAexyig==',
-        'AZURE_STORAGE_CLIENT_ID': 'd639bb9a-adf4-4f80-b950-ca9c4eed439d',
-        'AZURE_STORAGE_CLIENT_SECRET': '00f88568-d7d0-4003-9cef-a89092f1cc59',
-        'AZURE_STORAGE_TENANT_ID': '4089272c-0e47-4d4f-81f7-58eec2bdb4f1',
+        'AZURE_STORAGE_ACCOUNT_NAME': '<removidos_por_seguranca-estao_no_original>',
+        'AZURE_STORAGE_ACCOUNT_KEY': 'removidos_por_seguranca-estao_no_original',
+        'AZURE_STORAGE_CLIENT_ID': 'removidos_por_seguranca-estao_no_original',
+        'AZURE_STORAGE_CLIENT_SECRET': 'removidos_por_seguranca-estao_no_original',
+        'AZURE_STORAGE_TENANT_ID': 'removidos_por_seguranca-estao_no_original',
     }
 
-    uri = 'abfss://breweries@datalakebreweries.dfs.core.windows.net/Silver/'
+    uri = 'abfss://removidos_por_seguranca-estao_no_original/Gold/'
 
     write_deltalake(
         uri,
